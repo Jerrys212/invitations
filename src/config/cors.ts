1586ac2cc4688dabfbe302aaf3138d45455a5cbf
jerryG <jerryxd212@gmail.com>
2025-12-12 16:42:00 -0600

    fix bug

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,7 +2,12 @@ import { CorsOptions } from "cors";
 
 export const corsConfig: CorsOptions = {
     origin: (origin, callback) => {
-        const whitelist = [process.env.FRONTURL];
+        const whitelist = [process.env.FRONTURL, "http://localhost:5173", "http://localhost:3000"].filter(Boolean); // Elimina valores undefined
+
+        if (!origin) {
+            callback(null, true);
+            return;
+        }
 
         if (whitelist.includes(origin)) {
             callback(null, true);
